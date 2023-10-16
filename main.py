c3d6bb5d10750520cf86d6faa860ef7753c4c894
choyeun <choyeun0372@gmail.com>
2023-10-16 13:13:07 +0900

    환경변수 설정

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
 import os
 import subprocess
+import pytesseract as ts
+from PIL import Image
+
+import self
+from ppadb.client import Client as AdbClient
+
+deviceport = 53218
+
+# ADB 연결
+def adb_connect():
+    current_dir = os.path.dirname(__file__)
+    subprocess.run([current_dir + "\\lib\\platform-tools\\adb.exe", "start-server"])
+
+    client = AdbClient(host="127.0.0.1", port=5037)
+    client.remote_connect("localhost", int(deviceport))
+    adbdevice = client.device("localhost:" + str(deviceport))
+    if adbdevice is not None:
+        print("Adb detected")
+    else:
+        print("Adb not detected")
+        exit(0)
+    return adbdevice
+
+# Tesseract-OCR 환경변수 설정
+def create_env_var():
+    current_dir = os.path.dirname(__file__)
+    existing_value = os.environ.get("Path", "")
+    additional_value = ";" + current_dir + "\\lib\\Tesseract-OCR\\"
+    new_value = existing_value + additional_value
+    os.environ["Path"] = new_value
 
 
-# 현재 스크립트 파일의 디렉토리 경로를 얻기
-current_dir = os.path.dirname(__file__)  # 현재 스크립트 파일의 디렉토리 경로
+def __init__(self):
+    create_env_var()
+    return adb_connect()
 
+if __name__ == "__main__":
+    adbdevice = __init__(self)
+    current_dir = os.path.dirname(__file__)
+    x = 500
+    y = 300
+    cmd = "input touchscreen tap " + str(x) + " " + str(y)
+    adbdevice.shell(cmd)
+    subprocess.run([current_dir + "\\lib\\platform-tools\\adb.exe", "kill-server"])
+    img = Image.open('son.png')
+    img2 = Image.open('son2.png')
 
-subprocess.run([current_dir+"\\lib\\platform-tools\\adb.exe", "start-server"])
-print("현재 디렉토리:", current_dir)
-
-subprocess.run([current_dir+"\\lib\\platform-tools\\adb.exe", "kill-server"])
+    text = ts.image_to_string(img, lang='kor')
+    text2 = ts.image_to_string(img2, lang='eng')
+    print()
+    print(text)
+    print()
+    print(text2)
